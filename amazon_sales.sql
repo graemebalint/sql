fff7da1342af49ec7a573e7516b6507a2e2cea29
Graeme Balint <graeme.m.balint@gmail.com>
2023-02-08 12:40:39 -0500

    Update amazon_sales.sql

--- amazon_sales.sql
+++ amazon_sales.sql
@@ -1,7 +1,7 @@
 select product_id
-		, discounted_price
-		, actual_price, rating
-		, round(((cast((actual_price-discounted_price) as numeric)/actual_price)*100)) as percent_savings
+	, discounted_price
+	, actual_price, rating
+	, round(((cast((actual_price-discounted_price) as numeric)/actual_price)*100)) as percent_savings
 from amazon_sales;
 
 
@@ -9,7 +9,7 @@ from amazon_sales;
 
 
 select category
-		, round(cast(avg(rating) as numeric),1) as avg_rating
+	, round(cast(avg(rating) as numeric),1) as avg_rating
 from amazon_sales
 group by category
 order by avg_rating desc;
@@ -20,17 +20,17 @@ order by avg_rating desc;
 
 with temp as 
 	(select category
-			, round(cast(avg(rating) as numeric),1) as avg_rating
+		, round(cast(avg(rating) as numeric),1) as avg_rating
 	from amazon_sales
 	group by category)
 
 select category
-		, avg_rating
+	, avg_rating
 from temp
 where avg_rating in ((select max(avg_rating) from temp), (select min(avg_rating) from temp));
 
 select rating
-		, round(avg(((cast((actual_price-discounted_price) as numeric)/actual_price)*100))) as avg_percent_savings
+	, round(avg(((cast((actual_price-discounted_price) as numeric)/actual_price)*100))) as avg_percent_savings
 from amazon_sales
 group by rating
 order by rating desc;
@@ -40,7 +40,7 @@ order by rating desc;
 
 
 select category
-		, round(avg(((cast((actual_price-discounted_price) as numeric)/actual_price)*100))) as avg_percent_savings
+	, round(avg(((cast((actual_price-discounted_price) as numeric)/actual_price)*100))) as avg_percent_savings
 from amazon_sales
 group by category
 order by avg_percent_savings desc;
